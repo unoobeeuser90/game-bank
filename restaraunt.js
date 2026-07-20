f677147ed8c57d4844e535840a81216beb077da9
Aboba <unoobee.user90@gmail.com>
2026-07-20 15:52:40 +0300

    Update restaraunt.js

--- restaraunt.js
+++ restaraunt.js
@@ -3,8 +3,8 @@ from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 
 const supabase = createClient(
-"ТВОЙ_SUPABASE_URL",
-"ТВОЙ_ANON_KEY"
+"https://kvfytzxnlqxytqcsxkzq.supabase.co",
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt2Znl0enhubHF4eXRxY3N4a3pxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQzNzE3NTgsImV4cCI6MjA5OTk0Nzc1OH0.dlVGtE359MBEL-P53p6XD7u1js3Bq1FcFQI6-ifBZsk"
 );
 
 
